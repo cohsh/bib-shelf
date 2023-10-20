f198283742f3f01775b07484bfcfd4ff1d747ddd
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-20 14:42:41 +0900

    [feat] show window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use gtk::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let application =
+        gtk::Application::new(Some("com.github.cohsh.pdf-bib"), Default::default());
+    application.connect_activate(build_ui);
+    application.run();
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(application);
+    window.set_title(Some("PDF-bib"));
+    window.set_default_size(1200, 600);
+    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
+    window.set_child(Some(&vbox));
+    window.show();
 }
