25c785357a9a7d799c239413a843105a89414e2a
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-23 15:19:05 +0900

    [update] struct for new feats

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,31 @@ use paper::Paper;
 use bib::get_bib;
 use util::{mkdir, write};
 
+#[derive(Debug)]
+pub struct Bib {
+    model: gio::ListStore,
+}
+
+impl Default for Bib {
+    fn default() -> Self {
+        let model = gio::ListStore::new::<Paper>();
+
+        Self {
+            model,
+        }
+    }
+}
+
+impl Bib {
+    pub fn add_paper(&mut self, paper: &Paper) {
+        self.model.append(paper);
+    }
+
+    pub fn model(&self) -> &gio::ListStore {
+        &self.model
+    }
+}
+
 fn main() {
     let application = gtk::Application::new(Some("com.github.cohsh.pdf-bib"), Default::default());
     application.connect_activate(build_ui);
@@ -18,13 +43,12 @@ fn main() {
 
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
-    window.set_title(Some("PDF-bib"));
-
-    window.set_default_size(800, 600);
-
     let vbox = gtk::Box::new(gtk::Orientation::Vertical, 10);
 
-    let model = gio::ListStore::new::<Paper>();
+    window.set_title(Some("PDF-bib"));
+    window.set_default_size(800, 600);
+
+    let mut bib = Bib::default();
 
     let mut v_bib = get_bib("ref.bib".into());
 
@@ -33,7 +57,7 @@ fn build_ui(application: &gtk::Application) {
         let dir = "data/".to_string() + &v[3].clone();
         mkdir(dir.clone());
         let path_pdf = dir.clone() + "/" + &v[3].clone() + ".pdf";
-        model.append(&Paper::new(
+        bib.add_paper(&Paper::new(
             v[0].clone(),
             v[1].clone(),
             v[2].clone(),
@@ -44,14 +68,14 @@ fn build_ui(application: &gtk::Application) {
     }
 
     let list_box = gtk::ListBox::new();
-    list_box.bind_model(Some(&model), |item| {
+    list_box.bind_model(Some(bib.model()), |item| {
         let paper = item.downcast_ref::<Paper>().unwrap();
         ui::display_ui(paper).upcast::<gtk::Widget>()
     });
 
-    list_box.connect_row_activated(move |list_box, row| {
+    list_box.connect_row_activated(move |_list_box, row| {
         let index = row.index();
-        if let Some(item) = model.item(index as u32) {
+        if let Some(item) = bib.model().item(index as u32) {
             if let Some(paper) = item.downcast_ref::<Paper>() {
                 let pdf_path = paper.path();
                 println!("PDF path: {}", pdf_path);
@@ -71,6 +95,11 @@ fn build_ui(application: &gtk::Application) {
         .build();
 
     vbox.append(&scrolled_window);
+
+    let entry = gtk::Entry::builder()
+        .editable(true)
+        .build();
+    vbox.append(&entry);
 
     window.set_child(Some(&vbox));
     window.show();
