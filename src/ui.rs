b73fcfa6fa4085f4aaac408423c0cd3ddc632373
cohsh <82075327+cohsh@users.noreply.github.com>
2023-10-24 16:25:45 +0900

    [feat] if PDF exists, show ✅

--- src/ui.rs
+++ src/ui.rs
@@ -1,6 +1,7 @@
 use crate::Paper;
 use gtk;
 use gtk::prelude::*;
+use std::path::Path;
 
 pub fn display_ui(paper: &Paper) -> impl IsA<gtk::Widget> {
     let hbox = gtk::Box::builder()
@@ -33,11 +34,28 @@ pub fn display_ui(paper: &Paper) -> impl IsA<gtk::Widget> {
         .sync_create()
         .build();
 
-
     title.set_hexpand(true);
     author.set_hexpand(true);
     hbox.append(&year);
     hbox.append(&author);
     hbox.append(&title);
+
+    let path_str = paper.path();
+    let path = Path::new(&path_str);
+
+    if path.is_file() {
+        let path_label = gtk::Label::builder()
+            .label("✅")
+            .halign(gtk::Align::Start)
+            .build();
+        hbox.append(&path_label);
+    } else {
+        let path_label = gtk::Label::builder()
+        .label("⬜")
+        .halign(gtk::Align::Start)
+        .build();
+        hbox.append(&path_label);
+    }
+
     hbox
 }
